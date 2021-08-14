c3f2c04845bc16f1dfdb927264e2322c825fd4f8
inakicrack <clase.iaki.001@gmail.com>
2021-08-14 12:15:25 +0200

    Duda de nueva clase Pregunta
    No me van las variables probablemente por el constructor

---
+++ src/Pregunta.java
@@ -0,0 +1,12 @@
+
+public class Pregunta {
+
+	final String pregunta;
+	final String respuesta;
+	
+	Pregunta(){
+		this.pregunta = "¿Voy a aprobar el año que viene?";
+		this.respuesta = "SI";
+	}
+	
+}
--- src/Reto1_Scanner.java
+++ src/Reto1_Scanner.java
@@ -4,13 +4,12 @@ public class Reto1_Scanner {
 
 	public static void main(String[] args) {
 		Scanner leerLinea = new Scanner(System.in);
-		final String RESPUESTA_CORRECTA = "SI";
 		boolean esCorrecta = true;
 		
 		while (esCorrecta) {
-			System.out.println("¿Voy a aprobar el año que viene?");
-			String respuesta = leerLinea.nextLine().trim();
-			if (RESPUESTA_CORRECTA.toUpperCase().equals(respuesta.toUpperCase())) {
+			System.out.println(pregunta);
+			String respuestaDelUsuario = leerLinea.nextLine().trim();
+			if (respuesta.equals(respuestaDelUsuario.toUpperCase())) {
 				esCorrecta = false;
 				System.out.println("Yes, playa!");
 			}else {
