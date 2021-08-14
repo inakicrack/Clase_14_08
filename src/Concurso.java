f4ee0c0d4f7d1da2f00751e9cd1967ce1879bee6
inakicrack <clase.iaki.001@gmail.com>
2021-08-14 13:27:21 +0200

    Iniciando código con Concurso
    Empezando a usar Concurso

--- src/Concurso.java
+++ src/Concurso.java
@@ -1,4 +1,19 @@
 
 public class Concurso {
 
+	public final Pregunta listaDePreg[];
+	
+	Concurso(Pregunta lDP[]){
+		this.listaDePreg = lDP;
+	}
+	
+	public void crearConcurso() {
+		Pregunta preg1 = new Pregunta ("¿Voy a aprobar el año que viene?", "SI");
+		Pregunta preg2 = new Pregunta ("¿Va a ser difícil el año que viene?", "NO");
+		Pregunta preg3 = new Pregunta ("¿Vamos a por todas?", "SI");
+		Pregunta listaDePreg[] = new Pregunta [3];
+		listaDePreg[0] = preg1;
+		listaDePreg[1] = preg2;	
+		listaDePreg[2] = preg3;
+	}
 }
