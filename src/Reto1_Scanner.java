48916dcd58232f74d87df774e5f9aae44b38872e
inakicrack <clase.iaki.001@gmail.com>
2021-08-14 11:57:24 +0200

    Finalizado el bucle
    Terminado el bucle del Reto1

--- src/Reto1_Scanner.java
+++ src/Reto1_Scanner.java
@@ -5,13 +5,18 @@ public class Reto1_Scanner {
 	public static void main(String[] args) {
 		Scanner leerLinea = new Scanner(System.in);
 		final String RESPUESTA_CORRECTA = "SI";
+		boolean esCorrecta = true;
 		
-		System.out.println("¿Voy a aprobar el año que viene?");
-		String respuesta = leerLinea.nextLine().trim();
-		if (RESPUESTA_CORRECTA.toUpperCase().equals(respuesta.toUpperCase())) {
-			System.out.println("Yes, playa!");
-		}else {
-			System.out.println("Payaso!! Revisa las evidencias!!");
-		}
-	}	
+		while (esCorrecta) {
+			System.out.println("¿Voy a aprobar el año que viene?");
+			String respuesta = leerLinea.nextLine().trim();
+			if (RESPUESTA_CORRECTA.toUpperCase().equals(respuesta.toUpperCase())) {
+				esCorrecta = false;
+				System.out.println("Yes, playa!");
+			}else {
+				System.out.println("Quiero que lo repitas hasta que la respuesta sea correcta");	
+			}
+		}	
+	}
+	
 }
