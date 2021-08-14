c19b41e4ef747228f491c8c46c3212a1005304df
inakicrack <clase.iaki.001@gmail.com>
2021-08-14 11:38:46 +0200

    Terminado el Reto1
    Finiquitado, mira a ver si hace falta algo estético o de respuesta incorrecta

--- src/Reto1_Scanner.java
+++ src/Reto1_Scanner.java
@@ -3,17 +3,15 @@ import java.util.Scanner;
 public class Reto1_Scanner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-	
-	Scanner leerLinea = new Scanner(System.in);
-	final String RESPUESTA_CORRECTA = "Si";
-	
-	System.out.println("¿Voy a aprobar el año que viene?");
-	String respuesta = leerLinea.nextLine().trim();
-	if (RESPUESTA_CORRECTA.toupper().equals(respuesta.toupper)) {
+		Scanner leerLinea = new Scanner(System.in);
+		final String RESPUESTA_CORRECTA = "SI";
 		
-	}
-	
+		System.out.println("¿Voy a aprobar el año que viene?");
+		String respuesta = leerLinea.nextLine().trim();
+		if (RESPUESTA_CORRECTA.toUpperCase().equals(respuesta.toUpperCase())) {
+			System.out.println("Yes, playa!");
+		}else {
+			System.out.println("Payaso!! Revisa las evidencias!!");
+		}
+	}	
 }
