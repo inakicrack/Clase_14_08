1598cb74904a561591185ef10882d8c5adf70960
inakicrack <clase.iaki.001@gmail.com>
2021-08-14 13:55:32 +0200

    Funciona todo de nuevo
    Era el final y el this.listaDePreg

--- src/ConjuntoDePreguntas.java
+++ src/ConjuntoDePreguntas.java
@@ -1,7 +1,7 @@
 
 public class ConjuntoDePreguntas {
 
-	public final Pregunta listaDePreg[];
+	public Pregunta listaDePreg[];
 	
 	ConjuntoDePreguntas(Pregunta lDP[]){
 		this.listaDePreg = lDP;
@@ -15,9 +15,9 @@ public class ConjuntoDePreguntas {
 		Pregunta preg1 = new Pregunta ("¿Voy a aprobar el año que viene?", "SI");
 		Pregunta preg2 = new Pregunta ("¿Va a ser difícil el año que viene?", "NO");
 		Pregunta preg3 = new Pregunta ("¿Vamos a por todas?", "SI");
-		Pregunta listaDePreg[] = new Pregunta [3];
-		listaDePreg[0] = preg1;
-		listaDePreg[1] = preg2;	
-		listaDePreg[2] = preg3;
+		this.listaDePreg = new Pregunta [3];
+		this.listaDePreg[0] = preg1;
+		this.listaDePreg[1] = preg2;	
+		this.listaDePreg[2] = preg3;
 	}
 }
