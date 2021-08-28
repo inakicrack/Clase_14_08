539a7ea984c2b793ff2efd0d8d6ca7306f4d5ae7
inakicrack <clase.iaki.001@gmail.com>
2021-08-28 13:18:39 +0200

    Hecho saludoFinal
    Método que sirve para hacer el saludo final dependiendo de las respuestas acertadas

--- src/BaseDeDatosDePreguntasList3.java
+++ src/BaseDeDatosDePreguntasList3.java
@@ -10,7 +10,31 @@ public class BaseDeDatosDePreguntasList3 extends LinkedList<ConjuntoDePreguntasL
 		for(ConjuntoDePreguntasList3 conj: this) {
 			conj.preguntar(prop);
 		}
-		System.out.println("\n¡¡Se ha acabado el concurso!!\n" + "¡¡Has acertado " + prop.toString() + "!!");
-	}	
+		saludoFinal(prop);
+	}
+	
+	public void saludoFinal(Proporcion prop) {
+		switch(prop.getRespuestaAcertadas()) {
+			case 0:	
+				System.out.println("\n¡¡Se ha acabado el concurso!!\n" + "\nHas acertado " + prop.toString() + "...\n\n¡¡No has dado ni una!!");
+				break;
+			case 1:
+			case 2:
+			case 3:
+				System.out.println("\n¡¡Se ha acabado el concurso!!\n" + "\nHas acertado " + prop.toString() + "...\n\nLo sentimos, has perdido");
+				break;
+			case 4:
+			case 5:
+				System.out.println("\n¡¡Se ha acabado el concurso!!\n" + "\nHas acertado " + prop.toString() + "...\n\n¡Casi lo consigues!");
+				break;
+			case 6:
+				System.out.println("\n¡¡Se ha acabado el concurso!!\n" + "\n¡¡Has acertado " + prop.toString() + "!!\n\n¡¡Has ganado el concurso!!");
+				break;
+			default:
+				System.out.println("¿Que coño has hecho?");
+				break;
+		}
+		
+	}
 }
 
